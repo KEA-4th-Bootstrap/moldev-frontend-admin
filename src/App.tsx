e33b8738408bc035c79a9cef1497b072c69a98c5
Ginieee <kaj1226@naver.com>
2024-06-05 17:41:15 +0900

    ✨ [Feat] App 라우팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,40 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import UserListPage from './pages/UserListPage';
+import MainPage from './pages/MainPage';
+import ReportReceptionPage from './pages/ReportReceptionPage';
+import ReportProcessPage from './pages/ReportProcessPage';
+import useAuthStore from './store/useAuthStore';
+import { useEffect } from 'react';
+
 function App() {
-  return <div className="App">앱입니다</div>;
+  const { isLoggedIn } = useAuthStore();
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      if (window.location.pathname !== '/login')
+        window.location.href = '/login';
+    }
+  }, [isLoggedIn]);
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />}>
+          <Route index element={<Navigate replace to="/user/list" />} />
+          <Route path="user/list" element={<UserListPage />} />
+          <Route path="report/reception" element={<ReportReceptionPage />} />
+          <Route path="report/process" element={<ReportProcessPage />} />
+        </Route>
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
