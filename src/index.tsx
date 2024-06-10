d20338eb786a717c98404cb691f315e90fd8bada
Ginieee <kaj1226@naver.com>
2024-06-10 17:25:42 +0900

    ✨ [Feat] react-query 사용 위한 준비

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import './assets/fonts/font.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
-root.render(<App />);
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>,
+);
