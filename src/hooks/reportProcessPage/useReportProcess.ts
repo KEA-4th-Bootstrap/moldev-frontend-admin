d8dc59a479ea89dc4f5ac5ff164cf857f215880b
Ginieee <kaj1226@naver.com>
2024-06-10 17:24:29 +0900

    ✨ [Feat] ReportProcessPage 및 useReportProcess 훅 생성

--- src/hooks/reportProcessPage/useReportProcess.ts
+++ src/hooks/reportProcessPage/useReportProcess.ts
@@ -1,47 +1,71 @@
 import { useEffect, useState } from 'react';
 import { reportItemType } from '../../data/type';
+import { useQuery } from 'react-query';
+import { getProcessedReportList } from '../../api/reportApi';
 
 export const useReportProcess = () => {
-  const totalLength = 10;
   const placeHolder = '유저명으로 검색해주세요.';
   const [inputValue, setInputValue] = useState('');
-  const [filterOption, setFilterOption] = useState<{
-    order: 'reported' | 'processed';
-    filter: {
-      post: boolean;
-      comment: boolean;
-    };
-  }>({
-    order: 'reported',
-    filter: {
-      post: true,
-      comment: true,
-    },
+  const [filterOption, setFilterOption] = useState({
+    post: true,
+    comment: true,
   });
-  const [reportReceptionList, setReportReceptionList] = useState<
-    reportItemType[]
-  >([]);
-  const [currentIdx, setCurrentIdx] = useState(1);
+  const [totalLength, setTotalLength] = useState(0);
+  const [currentIdx, setCurrentIdx] = useState(0);
+
+  const {
+    data: reportProcessedList,
+    isLoading: reportProcessedListIsLoading,
+    isError: reportProcessedListIsError,
+  } = useQuery<reportItemType[]>(
+    ['reportProcessed', filterOption, inputValue, currentIdx],
+    () =>
+      getProcessedReportList({
+        type:
+          filterOption.post && filterOption.comment
+            ? null
+            : filterOption.post
+              ? 'POST'
+              : 'REPLY',
+        search: inputValue ? inputValue : null,
+        page: currentIdx,
+        size: 10,
+      }).then((res) => {
+        console.log('신고 처리 현황 리스트 조회 원본 데이터 --> ', res);
+        setTotalLength(res.data.data.pageInfo.totalElements);
+        return res.data.data.reportResponseVo;
+      }),
+    {
+      refetchOnWindowFocus: false,
+      retry: 2,
+      onSuccess: (data) => {
+        console.log('신고 처리 현황 리스트 조회 성공 --> ', data);
+      },
+      onError: (error) => {
+        console.log('신고 처리 현황 리스트 조회 실패 --> ', error);
+      },
+    },
+  );
 
   const onChangeInputValue = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
   };
 
-  const onHandleOrder = (order: 'reported' | 'processed') => {
-    setFilterOption({
+  const onChangeFilterOption = (option: 'post' | 'comment') => {
+    let newFilterOption = {
       ...filterOption,
-      order: order,
-    });
-  };
+      [option]: !filterOption[option],
+    };
 
-  const onHandleFilter = (option: 'post' | 'comment') => {
-    setFilterOption({
-      ...filterOption,
-      filter: {
-        ...filterOption.filter,
-        [option]: !filterOption.filter[option],
-      },
-    });
+    // Ensure at least one option is true
+    if (!newFilterOption.post && !newFilterOption.comment) {
+      newFilterOption = {
+        ...newFilterOption,
+        post: true,
+      };
+    }
+
+    setFilterOption(newFilterOption);
   };
 
   useEffect(() => {
@@ -53,11 +77,11 @@ export const useReportProcess = () => {
     inputValue,
     onChangeInputValue,
     filterOption,
-    onHandleOrder,
-    onHandleFilter,
+    onChangeFilterOption,
     totalLength,
-    reportReceptionList,
-    setReportReceptionList,
+    reportProcessedList,
+    reportProcessedListIsLoading,
+    reportProcessedListIsError,
     currentIdx,
     setCurrentIdx,
   };
