3896916edcb2e25b51d82a45e54cfbe8a96983a1
Ginieee <kaj1226@naver.com>
2024-06-10 17:24:54 +0900

    ✨ [Feat] 신고내역 조회 위한 useReportDetailModal 생성

--- src/hooks/common/useReportDetailModal.ts
+++ src/hooks/common/useReportDetailModal.ts
@@ -1,13 +1,76 @@
 import { useState } from 'react';
-import { reportItemType, reportProcessType } from '../../data/type';
+import {
+  reportItemType,
+  reportProcessType,
+  reportReasonKorToEng,
+} from '../../data/type';
+import { useMutation, useQuery } from 'react-query';
+import { getReportDetail, patchReport, postBanUser } from '../../api/reportApi';
 
-export const useReportDetailModal = (report: reportItemType) => {
+export const useReportDetailModal = (
+  report: reportItemType,
+  onClose: () => void,
+) => {
   console.log(report);
   const [processType, setProcessType] = useState<reportProcessType | null>(
     null,
   );
   const [isProcessOpen, setIsProcessOpen] = useState<boolean>(false);
 
+  const {
+    data: reportDetail,
+    isLoading,
+    isError,
+  } = useQuery<any>(
+    ['reportDetail', report.contentId, report.reportType],
+    () =>
+      getReportDetail(report.contentId, report.reportType).then((res) => {
+        console.log('신고 상세 조회 원본 데이터 ---> ', res);
+        return res.data.data;
+      }),
+    {
+      retry: 2,
+      refetchOnWindowFocus: false,
+      onSuccess: (data) => {
+        console.log('신고 상세 조회 성공 --> ', data);
+      },
+      onError: (error) => {
+        console.log('신고 상세 조회 실패 --> ', error);
+      },
+    },
+  );
+
+  const { mutate: tryBanUser } = useMutation(
+    () =>
+      postBanUser(
+        report.reportId,
+        report.reporteeId,
+        Number(processType),
+        reportReasonKorToEng[report.reason],
+      ),
+    {
+      onSuccess: (data) => {
+        console.log('유저 차단 성공 --> ', data);
+        onClose();
+      },
+      onError: (error) => {
+        console.log('유저 차단 실패 --> ', error);
+      },
+    },
+  );
+
+  const { mutate: cancelReport } = useMutation(
+    () => patchReport(report.reportId),
+    {
+      onSuccess: (data) => {
+        console.log('신고 반려 성공 --> ', data);
+        onClose();
+      },
+      onError: (error) => {
+        console.log('신고 반려 실패 --> ', error);
+      },
+    },
+  );
   const onProcessClick = () => {
     setIsProcessOpen(!isProcessOpen);
   };
@@ -17,10 +80,22 @@ export const useReportDetailModal = (report: reportItemType) => {
     setIsProcessOpen(false);
   };
 
+  const handleProcessButton = () => {
+    if (processType === 'none') {
+      cancelReport();
+    } else {
+      tryBanUser();
+    }
+  };
+
   return {
     processType,
     isProcessOpen,
     onProcessClick,
     handleProcessType,
+    reportDetail,
+    isLoading,
+    isError,
+    handleProcessButton,
   };
 };
